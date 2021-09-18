85b162cefae884e7a78361563c9160cf6f21e273
GCreeper00 <liueric390@gmail.com>
2021-09-18 18:34:04 -0400

    App.vue now resizes div to have the same resolution Resolutions stays the same, but div scales to fit the screen, and is centered

---
+++ client-side/src/components/StartScreen.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="start-screen">
+    <h1></h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "StartScreen",
+  data() {
+    return {
+
+    }
+  },
+  computed: {
+
+  }
+}
+</script>
+
+<style scoped>
+#start-screen {
+  width: 50%;
+  height: 50%;
+  background-color: antiquewhite;
+}
+</style>
